59ce29e4e3bc08e59dca5e8d532544292c8a4bb5
JeffreyDeras <jefreyjb10@gmail.com>
2023-05-08 17:21:18 -0300

    Tem que mudar o codigo

--- Objetos/aula.js
+++ Objetos/aula.js
@@ -1,4 +1,73 @@
+class pessoa {
+        constructor(nome, idade) {
+                this.nome = nome;
+                this.idade = idade;
+        }}
+
+
+function compararPessoas(pessoa1, pessoa2) {
+        
+if (pessoa1.idade > pessoa2.idade) {
+        console.log(pessoa1.nome + 'é mais velhor que' + pessoa2.nome);
+}else if(pessoa1.idade < pessoa2.idade){
+        console.log(pessoa1.nome + ' é mais nova que ' + pessoa2.nome);
+}else{
+        console.log('${pessoa1.nome}');
+}
+}
+
+const pessoa1 = new pessoa('Jeffrey', 32);
+const pessoa2 = new pessoa('Andrew', 32);
+
+compararPessoas(pessoa1, pessoa2);
+
+
+
+
+
+
+
+
+
+
+/** Clasee-Exemplo-Principal*/
+/*
+
+class pessoa {  
+        constructor(nome, idade, anoDeNascimento, sexo) { 
+                this.nome = nome;
+                this.idade = idade;
+                this.anoDeNascimento = 2023 - idade;
+                this.sexo = 'Masculino'; 
+        }
+
+        descrever (){
+                console.log(`Meu nome é ${this.nome} e minha idade ${this.idade} nasci em ${this.anoDeNascimento} do sexo ${this.sexo}`);
+        }
+}
+
+const pessoa1 = new pessoa('Jeffrey', 32,  2023, 'Masculino');
+const pessoa2 = new pessoa('Andrew', 25,  2023, 'Masculino');
+
+
+console.log(pessoa1);
+
+pessoa1.descrever();
+
+console.log(pessoa2);
+
+pessoa2.descrever();
+*/
+/**End-Exemplo principal */
+
+
+
+
+
+
+
 /*Sintaxe-normal*/
+/*
 const pessoa = {
         nome: 'Jeffrey',
         idade: 32,
@@ -13,13 +82,15 @@ pessoa.descrever = function () {
 
 }
 pessoa.descrever();
-
+*/
 /**End-Sintaxe-normal **/
 
 
 
-/**Sintaxe-mais-dinamica */
 
+
+/**Sintaxe-mais-dinamica */
+/*
 const pessoa1 = {
         nome: 'Jeffrey Deras',
         idade: 32,
@@ -35,6 +106,10 @@ pessoa1.descrever = function () {
 }
 pessoa1.descrever ();
 
+*/
+
 /**End-Sintaxe-mais-dinamica **/
+
+
 
 
